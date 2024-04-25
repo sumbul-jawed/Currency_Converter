01320e12e1403929a3133a761f209056ca25334d
Sumbul_Jawed <160109823+sumbul-jawed@users.noreply.github.com>
2024-04-25 12:52:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.blue.bold("\n Welcome to sumbul currency_convert Application\n"));
+// Define the list of currency and their exchange.
+let exchange_rate = {
+    "USD": 1,
+    "EUR": 0.9,
+    "JYP": 113,
+    "AUD": 1.65,
+    "CAD": 1.3,
+    "PKR": 277.70,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message: (chalk.greenBright("Select the currency to convert from:")),
+        choices: ["USD", "EUR", "JYP", "AUD", "CAD", "PKR"]
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: (chalk.yellow("Select the currency to convert into:")),
+        choices: ["USD", "EUR", "JYP", "AUD", "CAD", "PKR"]
+    },
+    {
+        name: "amount",
+        type: "input",
+        message: "Enter the  amount to convert:"
+    }
+]);
+let from_amount = exchange_rate[user_answer.from_currency];
+let to_amount = exchange_rate[user_answer.to_currency];
+let amount = user_answer.amount;
+let base_amount = amount / from_amount;
+let converted_amount = base_amount * to_amount;
+console.log(`converted Amount = ${chalk.redBright(converted_amount.toFixed(2))}`);
